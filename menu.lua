c5c9737a19e123ec3ace9b1476a2512b9f5188d5
hackgamevip <nguyendoconghau2k6@gmail.com>
2026-02-26 15:00:59 +0700

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -404,8 +404,21 @@ end, "🌐 ĐỔI SERVER", Theme.Brand, function()
 end)
 
 createButton(page2, "💻 Lệnh admin", Theme.AccentOn, function() pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))() end) end)
-createButton(page2, "🔨 LẤY BTOOLS", Theme.Brand, function() pcall(function() loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/BTools.txt"))() end) end)
-createButton(page2, "🕊️ FLY (Bay)", Theme.Brand, function() pcall(function() loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")() end) end)
+
+-- ĐÃ ĐỔI SCRIPT BTOOLS SANG PHIÊN BẢN ỔN ĐỊNH HƠN
+createButton(page2, "🔨 LẤY BTOOLS", Theme.Brand, function() 
+    pcall(function() 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe/main/F3X%20BTools"))() 
+    end) 
+end)
+
+-- ĐÃ ĐỔI SCRIPT FLY SANG PHIÊN BẢN CÓ BẢNG ĐIỀU KHIỂN
+createButton(page2, "🕊️ FLY (Bay)", Theme.Brand, function() 
+    pcall(function() 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.lua"))() 
+    end) 
+end)
+
 createButton(page2, "📂 TP SAVE V2", Theme.Brand, function() pcall(function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/0Ben1/fe/main/Tp%20Place%20GUI'),true))() end) end)
 
 -- [TAB 3: VỊ TRÍ TP SAVE]
